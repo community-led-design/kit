5e4445b84b793ad8f689a529744aaa9321dbee86
Ned Zimmerman <ned@bight.dev>
2021-02-09 16:03:26 -0400

    feat: add pullquote editor component

---
+++ src/admin/pullquote.js
@@ -0,0 +1,37 @@
+
+/* global CMS */
+
+"use strict";
+
+CMS.registerEditorComponent({
+    id: "pullquote",
+    label: "Pullquote",
+    fields: [
+        {
+            name: "quote",
+            label: "Quote",
+            widget: "markdown"
+        },
+        {
+            name: "author",
+            label: "Author",
+            widget: "string"
+        }
+    ],
+    pattern: /^{% pullquote "([\s\S]*?)" %}([\s\S]*?){% pullquote %}/,
+    fromBlock: function (match) {
+        return {
+            author: match[1],
+            quote: match[2]
+        };
+    },
+    toBlock: function (obj) {
+        return `{% pullquote "${obj.author}" %}\n${obj.quote}\n{% pullquote %}`;
+    },
+    toPreview: function (obj) {
+        var md = window.markdownit();
+        var quote = obj.quote ? md.render(obj.quote) : "";
+        var citation = obj.author ? `<cite>${obj.author}</cite>` : "";
+        return `<blockquote>${quote}${citation}</blockquote>`;
+    }
+});
