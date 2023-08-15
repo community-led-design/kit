6de3d086a2b0258b7e1fa122093b6fd281a2ec82
Ned Zimmerman <ned@bight.dev>
2023-08-15 09:36:51 -0300

    feat: use local assets for CMS

--- eleventy.config.js
+++ eleventy.config.js
@@ -13,7 +13,6 @@ https://github.com/inclusive-design/codesign.inclusivedesign.ca/raw/main/LICENSE
 "use strict";
 
 const path = require("path");
-
 const fluidPlugin = require("eleventy-plugin-fluid");
 const rssPlugin = require("@11ty/eleventy-plugin-rss");
 const navigationPlugin = require("@11ty/eleventy-navigation");
@@ -51,8 +50,6 @@ function imageShortcode(src, alt, sizes, widths) {
 const parseTransform = require("./src/_transforms/parse-transform.js");
 
 module.exports = function (eleventyConfig) {
-    eleventyConfig.setUseGitIgnore(false);
-
     // Shortcodes
     eleventyConfig.addPairedShortcode("accordion", content => {
         return `<div class="accordion flow">\n${content}\n</div>`;
@@ -60,7 +57,6 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPairedShortcode("example", exampleBlockShortcode);
     eleventyConfig.addPairedShortcode("learning", learningBlockShortcode);
     eleventyConfig.addPairedShortcode("pullquote", pullquoteShortcode);
-
     eleventyConfig.addNunjucksShortcode("resizeImage", imageShortcode);
 
     // Transforms
@@ -73,6 +69,14 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy({"src/assets/media": "assets/media"});
     eleventyConfig.addPassthroughCopy("src/admin/config.yml");
     eleventyConfig.addPassthroughCopy("src/admin/*.js");
+    eleventyConfig.addPassthroughCopy({
+        "node_modules/netlify-cms/dist/netlify-cms.js": "lib/cms/netlify-cms.js",
+        "node_modules/@babel/standalone/babel.min.js": "lib/cms/babel.min.js",
+        "node_modules/markdown-it/dist/markdown-it.min.js": "lib/cms/markdown-it.min.js",
+        "node_modules/prop-types/prop-types.min.js": "lib/cms/prop-types.min.js",
+        "node_modules/react/umd/react.development.js": "lib/cms/react.development.js",
+        "node_modules/react/umd/react.production.min.js": "lib/cms/react.production.min.js"
+    });
     eleventyConfig.addPassthroughCopy({"node_modules/infusion/src/lib/hypher/patterns": "lib/infusion/src/lib/hypher/patterns"});
 
     // Plugins
